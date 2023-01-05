23bb42ef4c033894ee3c4311ef9b010a8ff2eeb8
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-01-05 22:43:25 +0000

    added generateRandomString function, reorganized file for readability

--- express_server.js
+++ express_server.js
@@ -3,8 +3,17 @@ const app = express();
 const PORT = 8080; // default port 8080
 // setting EJS as templating engine
 app.set("view engine", "ejs");
+
+//////////////////////////////////////////////
+/////////////// MIDDLEWARE //////////////////
+//////////////////////////////////////////////
+
 // converts request body into readable string
 app.use(express.urlencoded({ extended: true }));
+
+////////////////////////////////////////////
+/////////////// DATABASE //////////////////
+///////////////////////////////////////////
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -13,10 +22,42 @@ const urlDatabase = {
   fart: 'asdfasdf'
 };
 
+////////////////////////////////////////////
+/////////////// FUNCTIONS //////////////////
+///////////////////////////////////////////
+
+const generateRandomString = function (length) {
+  return Math.random().toString(36).substr(2, length)
+};
+generateRandomString(6);
+
+
+////////////////////////////////////////////
+/////////////// ROUTES //////////////////
+///////////////////////////////////////////
+
 // registers a handler on the root path '/'
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
+
+//////////////////////////////////////////////
+////////////// CREATE OPERATIONS/////////////////
+//////////////////////////////////////////////
+
+// a GET route to render the urls_new.ejs template in the browser, to present the form to the user
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+//////////////////////////////////////////////
+////////////// READ OPERATIONS/////////////////
+//////////////////////////////////////////////
 
 // registers handler for the path /urls.json
 app.get('/urls.json', (req, res) => {
@@ -29,10 +70,6 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-//a GET route to render the urls_new.ejs template in the browser, to present the form to the user
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
 
 // GET route for single URL
 app.get("/urls/:id", (req, res) => {
@@ -46,6 +83,10 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+
+///////////////////////////////////////////////
+////// SERVER LISTENING ///////////////////////
+///////////////////////////////////////////////
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
