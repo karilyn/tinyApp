25408be899709dce41be4a988b0d9f778aae9933
karilyn <karilyn.kempton@gmail.com>
2023-01-13 15:55:00 -0800

    added two chai assertion tests

---
+++ test/helpersTest.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', () => {
+  it('should return a user object for an email that exists in the databse', () => {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.deepEqual(getUserByEmail(user, expectedUserID, true));
+  });
+
+  it('should return undefined for email that does not exist in database', () => {
+    const user = getUserByEmail("test@example.com", testUsers);
+    const expectedUserID = "undefined"
+    assert.deepEqual(getUserByEmail(user, expectedUserID, true));
+  });
+});
