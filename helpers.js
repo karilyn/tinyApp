49b4f1c680f30364c077e7dd17675af7064dce00
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-01-14 00:31:58 +0000

    added all helper functions to helpers module

--- helpers.js
+++ helpers.js
@@ -9,6 +9,25 @@ const getUserByEmail = function(email, users) {
   return;
 };
 
+// random string generator for generating short URL and userID
+const generateRandomString = function () {
+  return Math.random().toString(36).slice(2, 8);
+};
+
+// function to return URLS where the userId is equal to id of logged in user
+const getUrlsForUser = function (userId) {
+  let urls = {};
+  for (let id in urlDatabase) {
+    if (userId === urlDatabase[id].userId) {
+      urls[id] = urlDatabase[id];
+    }
+  }
+  return urls;
+};
+
+
 module.exports = {
   getUserByEmail,
+  generateRandomString,
+  getUrlsForUser,
 };
